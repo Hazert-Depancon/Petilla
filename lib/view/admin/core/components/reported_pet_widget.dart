747c2005f621e4937d5e1279d43bc88912ddb1f1
kagangullu <oguzkagangullu@gmail.com>
2022-11-26 11:19:27 +0300

    26.11.2022

--- lib/view/admin/core/components/reported_pet_widget.dart
+++ lib/view/admin/core/components/reported_pet_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:petilla_app_project/view/admin/core/models/reported_pet_model.dart';
 
 class ReportedPetWidget extends StatelessWidget {
@@ -9,11 +8,7 @@ class ReportedPetWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(double.parse(petModel.lat), double.parse(petModel.long)),
-        ),
-      ),
+      child: 
     );
   }
 }
