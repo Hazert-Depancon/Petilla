ee160246d9bf83e2825d8e9f2b3a8a1f889a0200
kagangullu <oguzkagangullu@gmail.com>
2022-11-26 13:36:20 +0300

    26.11.2022

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:petilla_app_project/core/constants/enums/locale_keys_enum.dart';
 import 'package:petilla_app_project/core/init/cache/locale_manager.dart';
 import 'package:petilla_app_project/core/init/lang/language_manager.dart';
 import 'package:petilla_app_project/core/init/theme/light_theme/light_theme.dart';
+import 'package:petilla_app_project/core/init/theme/light_theme/light_theme_colors.dart';
 import 'package:petilla_app_project/view/user/auth/onboard/view/onboarding.dart';
 import 'package:petilla_app_project/view/user/auth/view/login_view.dart';
 import 'package:petilla_app_project/view/user/start/view/select_app_view.dart';
@@ -38,7 +39,9 @@ void _initSystemUi() {
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
-      statusBarBrightness: Brightness.dark,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: LightThemeColors.scaffoldBackgroundColor,
+      systemNavigationBarIconBrightness: Brightness.dark,
     ),
   );
 }
