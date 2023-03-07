44132f1eb4c75c07719ba8f7e78475d666e382ba
kagangullu <oguzkagangullu@gmail.com>
2023-03-07 22:00:52 +0300

    init new desing template

--- lib/view/user/start/view/select_app_view.dart
+++ lib/view/user/start/view/select_app_view.dart
@@ -4,17 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:petilla_app_project/core/base/view/base_view.dart';
 import 'package:petilla_app_project/core/components/banner_ad_widget.dart';
+import 'package:petilla_app_project/core/constants/sizes_constant/project_padding.dart';
 import 'package:petilla_app_project/core/extension/string_lang_extension.dart';
 import 'package:petilla_app_project/core/init/lang/locale_keys.g.dart';
 import 'package:petilla_app_project/core/constants/sizes_constant/app_sized_box.dart';
-import 'package:petilla_app_project/core/constants/sizes_constant/project_padding.dart';
 import 'package:petilla_app_project/core/init/theme/light_theme/light_theme_colors.dart';
 import 'package:petilla_app_project/core/gen/assets.gen.dart';
-import 'package:petilla_app_project/view/user/apps/help_me/view/help_me_control.dart';
-import 'package:petilla_app_project/view/user/apps/main_petilla/view/main_petilla.dart';
-import 'package:petilla_app_project/view/user/apps/pet_form/main_pet_form.dart';
-import 'package:petilla_app_project/view/user/apps/petcook/view/petcook_control_view.dart';
-import 'package:petilla_app_project/view/user/start/core/components/select_app_widget.dart';
 import 'package:petilla_app_project/view/user/start/viewmodel/select_app_view_view_model.dart';
 
 class SelectAppView extends StatefulWidget {
@@ -61,41 +56,58 @@ class _SelectAppViewState extends State<SelectAppView> {
 
   SafeArea _body(context) {
     return SafeArea(
-      child: SingleChildScrollView(
-        padding: ProjectPaddings.horizontalMainPadding,
-        child: Column(
-          children: [
-            SelectAppWidget(
-              title: LocaleKeys.appNames_petilla.locale,
-              description: LocaleKeys.appDescription_petillaDescription.locale,
-              imagePath: Assets.images.petillaImage.path,
-              onTap: const MainPetilla(),
-            ),
-            mainSizedBox,
-            SelectAppWidget(
-              title: LocaleKeys.appNames_petform.locale,
-              description: LocaleKeys.appDescription_petformDescription.locale,
-              imagePath: Assets.images.petform.path,
-              onTap: const MainPetForm(),
-            ),
-            mainSizedBox,
-            SelectAppWidget(
-              title: LocaleKeys.appNames_helpMe.locale,
-              description: LocaleKeys.appDescription_helpMeDescription.locale,
-              imagePath: Assets.images.helpMe.path,
-              onTap: const HelpMeControl(),
-            ),
-            mainSizedBox,
-            SelectAppWidget(
-              title: LocaleKeys.appNames_petie.locale.locale,
-              description: LocaleKeys.appDescription_petieDescription.locale,
-              imagePath: Assets.images.petCookImage.path,
-              onTap: const PetCookControlView(),
-            ),
-          ],
-        ),
+        child: GridView(
+      padding: ProjectPaddings.horizontalMainPadding,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 2,
       ),
-    );
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+        Container(
+          color: Colors.amber,
+        ),
+      ],
+    )
+        // child: Column(
+        //   children: [
+        //     SelectAppWidget(
+        //       title: LocaleKeys.appNames_petilla.locale,
+        //       description: LocaleKeys.appDescription_petillaDescription.locale,
+        //       imagePath: Assets.images.petillaImage.path,
+        //       onTap: const MainPetilla(),
+        //     ),
+        //     mainSizedBox,
+        //     SelectAppWidget(
+        //       title: LocaleKeys.appNames_petform.locale,
+        //       description: LocaleKeys.appDescription_petformDescription.locale,
+        //       imagePath: Assets.images.petform.path,
+        //       onTap: const MainPetForm(),
+        //     ),
+        //     mainSizedBox,
+        //     SelectAppWidget(
+        //       title: LocaleKeys.appNames_helpMe.locale,
+        //       description: LocaleKeys.appDescription_helpMeDescription.locale,
+        //       imagePath: Assets.images.helpMe.path,
+        //       onTap: const HelpMeControl(),
+        //     ),
+        //     mainSizedBox,
+        //     SelectAppWidget(
+        //       title: LocaleKeys.appNames_petie.locale.locale,
+        //       description: LocaleKeys.appDescription_petieDescription.locale,
+        //       imagePath: Assets.images.petCookImage.path,
+        //       onTap: const PetCookControlView(),
+        //     ),
+        //   ],
+        // ),
+        // ),
+        );
   }
 
   GestureDetector _profileAction(BuildContext context) {
