87da6a8d0cb069710584739789dfbcb36746a4a7
kagangullu <oguzkagangullu@gmail.com>
2023-04-05 14:01:42 +0300

    select app view model edit

--- lib/feature/select_app/viewmodel/select_app_view_view_model.dart
+++ lib/feature/select_app/viewmodel/select_app_view_view_model.dart
@@ -1,9 +1,10 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api, deprecated_member_use
 
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:patily/core/base/model/base_viewmodel.dart';
 import 'package:patily/core/base/view/profile_view.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 part 'select_app_view_view_model.g.dart';
 
@@ -23,5 +24,19 @@ abstract class _SelectAppViewViewModelBase with Store, BaseViewModel {
   void callProfileView() {
     Navigator.push(viewModelContext,
         MaterialPageRoute(builder: (context) => const ProfileView()));
+  }
+
+  @action
+  Future<void> instagramLaunch() async {
+    var url = 'https://www.instagram.com/patily.turkiye/';
+
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        universalLinksOnly: true,
+      );
+    } else {
+      throw 'There was a problem to open the url: $url';
+    }
   }
 }
